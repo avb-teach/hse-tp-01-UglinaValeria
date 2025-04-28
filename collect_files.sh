31fb597acbf49494df3326c4265b51dbbae282e2
UglinaValeria <Uvalery08@mail.ru>
2025-04-29 00:33:48 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-chmod +x dcollect_files.sh
+chmod +x collect_files.sh
 set -e
+
+if [[ "$#" -lt 2 ]]; then
+  echo "Usage: $0 /path/to/input_dir /path/to/output_dir [--max_depth N]"
+  exit 1
+fi
 
 INPUT_DIR="$1"
 OUTPUT_DIR="$2"
@@ -12,6 +17,12 @@ if [[ "$1" == "--max_depth" && -n "$2" ]]; then
   MAX_DEPTH="$2"
 fi
 
+if [[ ! -d "$INPUT_DIR" ]]; then
+  echo "Error: Input directory '$INPUT_DIR' does not exist."
+  exit 1
+fi
+
+mkdir -p "$OUTPUT_DIR"
 
 if [[ -n "$MAX_DEPTH" ]]; then
   find "$INPUT_DIR" -mindepth 1 -maxdepth "$MAX_DEPTH" -type f
@@ -32,7 +43,7 @@ fi | while read -r filepath; do
   newname="$filename"
   counter=1
 
-
+ 
   while [[ -e "$OUTPUT_DIR/$newname" ]]; do
     newname="${name}${counter}${ext}"
     ((counter++))
